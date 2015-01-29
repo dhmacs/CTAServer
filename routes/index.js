5dc3440e92a862a2c82432f693226ba9d7ab46df
Macs <massimodemarchi91@gmail.com>
2015-01-29 11:41:43 -0600

    configuring

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pg = require('pg');
+var DATABASE_URL = 'postgres://mzbciewxfguoad:XwdC3c96EQvT9kEksokj1JDvn8@ec2-184-73-165-193.compute-1.amazonaws.com:5432/df78k0v3mkb016';
 var router = express.Router();
 
 /* GET home page. */
@@ -11,7 +13,30 @@ router.get('/ciao/', function(req, res, next) {
 });
 
 router.get('/jsonp/', function(req, res, next) {
-  res.jsonp({ title: 'boh2' });
+  var result;
+  pg.connect(DATABASE_URL, function(err, client) {
+    var query = client.query('SELECT * FROM your_table');
+
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+  });
+
+  res.jsonp({ result: 'boh2' });
+});
+
+router.get('/create/', function(req, res, next) {
+  var result;
+  pg.connect(DATABASE_URL, function(err, client) {
+    var query = client.query('CREATE TABLE example(greet varchar(255))');
+
+    /*
+    query.on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });*/
+  });
+
+  res.jsonp({ result: "ok"});
 });
 
 module.exports = router;
