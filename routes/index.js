47fadf28cef13c501e1f822e575b9260fa8710f5
Macs <massimodemarchi91@gmail.com>
2015-01-28 18:01:28 -0600

    first

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/ciao/', function(req, res, next) {
+  res.render('index', { title: 'Ciao' });
+});
+
+router.get('/jsonp/', function(req, res, next) {
+  res.jsonp({ title: 'Ciao' });
+});
+
+module.exports = router;
