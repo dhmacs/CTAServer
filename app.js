e635fd6d65903168edd767f4a2f084fd5a5dae6c
Macs <massimodemarchi91@gmail.com>
2015-01-30 10:09:07 -0600

    configuring

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // CSV
+var multer = require('multer');
+var uploads = require('./routes/uploads');
 var csv = require('ya-csv');
 
 // view engine setup
@@ -22,12 +24,13 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: "./uploads/"}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use("/uploads", uploads);
 
 
 // catch 404 and forward to error handler
