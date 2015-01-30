39ac7395364639f3c85837df5e5308d27d80080b
Macs <massimodemarchi91@gmail.com>
2015-01-30 00:49:22 -0600

    configuring

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ app.post('/upload', function(req, res) {
     var name = req.body.name,
         color = req.body.color;
     */
-    res.send(req.files);
+    req.form.on('end', function() {
+        res.send(req.files);
+    });
+
 });
 
 // catch 404 and forward to error handler
